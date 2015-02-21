75b1f2fd13e5ca07c07ce42acc76402684b07468
bkites <trainees13@gmail.com>
2015-02-21 05:34:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BPZ::Application.config.secret_key_base = 'b3b2aaef9f00c335c4be90ab0caa3afff39a124fcd5851cc9c799571fed54313d229b55b017f30b7a032cf3dc59c8fd6a18ca4432dd66be58c4856eeae5175f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BPZ::Application.config.session_store :cookie_store, key: '_BPZ_session'
